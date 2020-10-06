f3d9f0ce6d0662f79414d27d8124b8915733044e
fikri <mfikri102@outlook.com>
2020-10-06 16:05:53 +0700

    Remove timer to publish messages

--- main/app_driver.c
+++ main/app_driver.c
@@ -10,24 +10,9 @@
 #define BUTTON_ACTIVE_LEVEL 0
 #define RELAY_IO_NUM 4
 #define LED_IO_NUM 33
-#define TIMER_TIMEOUT_US 1000000
 
 static relay_handle_t relay_handle;
 static led_handle_t led_handle;
-static esp_timer_handle_t publish_timer;
-
-static void publish_timer_cb(void *arg)
-{
-    cloud_publish();
-}
-
-static void publish_timer_init(void)
-{
-    esp_timer_create_args_t publish_timer_args = {
-        .callback = &publish_timer_cb,
-    };
-    ESP_ERROR_CHECK(esp_timer_create(&publish_timer_args, &publish_timer));
-}
 
 static void button_tap_cb(void *arg)
 {
@@ -64,13 +49,7 @@ void relay_set_state(bool state)
         led_off();
     }
 
-    esp_err_t err = esp_timer_start_once(publish_timer, TIMER_TIMEOUT_US);
-    if (err != ESP_OK)
-    {
-        ESP_ERROR_CHECK(esp_timer_stop(publish_timer));
-        err = esp_timer_start_once(publish_timer, TIMER_TIMEOUT_US);
-    }
-    ESP_ERROR_CHECK(err);
+    cloud_publish();
 }
 
 bool relay_get_state(void)
@@ -130,6 +109,5 @@ void driver_init(void)
     iot_button_set_evt_cb(button_handle, BUTTON_CB_RELEASE, button_tap_cb, NULL);
     iot_button_add_custom_cb(button_handle, 5, button_press_5s_cb, NULL);
 
-    publish_timer_init();
     rmt_tx_init();
 }
